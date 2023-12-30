f89ddf50fd353d3a051df9169594115f98d30e21
jadepegasus <jadepegasus235@gmail.com>
2023-12-30 21:54:07 +0700

    ba

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 import { host } from './env';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : host;
 
-export const socket = io(URL);
+
+export const socket = io('https://frontend-social-r1ky.onrender.com');
